de72212edf0ad6ddc5d7c2c1a0d52576f4177955
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-12 16:13:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+n1 = int(input("Escreva o Primeiro número: "))
+n2 = int(input("Escreva o Segundo número: "))
+n3 = int(input("Escreva o Terceiro número:"))
+
+soma = n1+n2+n3
+
+print("Resultado da soma dos 3 Números é: ",soma)
